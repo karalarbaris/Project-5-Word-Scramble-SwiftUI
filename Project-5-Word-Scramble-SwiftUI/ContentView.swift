03c2bedb0161b1184083e603a52939402e3f5d12
Baris <karalarbaris@gmail.com>
2021-06-14 10:25:58 +0200

    Initial Commit

---
+++ Project-5-Word-Scramble-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project-5-Word-Scramble-SwiftUI
+//
+//  Created by Baris Karalar on 14.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project-5-Word-Scramble-SwiftUI/Project_5_Word_Scramble_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_5_Word_Scramble_SwiftUIApp.swift
+//  Project-5-Word-Scramble-SwiftUI
+//
+//  Created by Baris Karalar on 14.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct Project_5_Word_Scramble_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
